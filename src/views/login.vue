6a1847e899b570cf7762759c2aacf61862dd7842
GabrielRML <gabrielmirandaricardo@gmail.com>
2024-04-29 18:23:54 -0300

    fix: logo centralizado na tela de login

--- src/views/login.vue
+++ src/views/login.vue
@@ -44,13 +44,13 @@ export default {
 
 <template>
     <div class="d-flex h-100">
-        <div class="col-md-6 col-lg-6 col-xl-6 col-0 login-left">
+        <div class="d-flex flex-column justify-content-center align-items-center col-md-6 col-lg-6 col-xl-4 col-0 login-left">
             <div class="col-md-8 col-lg-8 col-xl-8">
                 <img src="/logoSemFundo.svg"
                 class="img-fluid larger-image" alt="Logo">
             </div>
-        </div>
-        <div class="col-md-6 col-lg-6 col-xl-6 col-12 d-flex flex-column justify-content-center">
+        </div> 
+        <div class="col-md-6 col-lg-6 col-xl-8 col-12 d-flex flex-column justify-content-center">
             <div class="d-flex flex-column align-items-center justify-content-between">
                 <div class="title">
                     <h1>Login</h1>
@@ -58,14 +58,14 @@ export default {
             
                 <div class="col-md-12 col-lg-12 col-xl-12 col-12">
                     <div class="d-flex flex-column justify-content-between align-items-center">
-                        <div class="col-md-8 col-lg-8 col-xl-8 col-8" style="margin-top: 2%;">
+                        <div class="col-md-8 col-lg-8 col-xl-4 col-8" style="margin-top: 2%;">
                             <FloatLabel>
                                 <InputText id="email" v-model="this.dados.email" :style="{ width: '100%' }" />
                                 <label for="email">Email</label>
                             </FloatLabel>
                         </div>
                 
-                        <div class="col-md-8 col-lg-8 col-xl-8 col-8" style="margin-top: 3%;">
+                        <div class="col-md-8 col-lg-8 col-xl-4 col-8" style="margin-top: 3%;">
                             <FloatLabel>
                                 <InputText id="senha" v-model="this.dados.senha" :style="{ width: '100%' }" />
                                 <label for="senha">Senha</label>
